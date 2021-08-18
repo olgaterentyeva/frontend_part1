557dd22e5a17b281697e5a72880f23ed2b88ba2b
terentyevaolga <terentyeevaolga@gmail.com>
2021-08-18 19:55:26 +0300

    begin

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1 Переменные
+// от англ variable
+
+// const firstName = 'Olga'
+// const lastname = 'Terentyeva'
+// const age = 19
+//
+// console.log('Имя студента: ' + firstName + ', а возраст: ' + age)
+
+const name = 'Olga'
+
+console.log(name)
+
+prompt('Введите фамилию')
